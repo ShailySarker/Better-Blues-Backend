0b938e5a8084238dd34d2becd10812ca97ec2d65
ShailySarker <shailysarker99@gmail.com>
2025-04-16 22:49:01 +0600

    initial setup completed

---
+++ app.js
@@ -0,0 +1,16 @@
+const dotenv = require ('dotenv');
+const express = require ('express');
+const cors = require ('cors');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, () => {
+    console.log(`Better Blue running on port ${PORT}`);
+});
