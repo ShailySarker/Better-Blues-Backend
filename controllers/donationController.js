96475e545be56b3e28609a7521ed80f2900fef4f
ShailySarker <shailysarker99@gmail.com>
2025-04-18 22:35:57 +0600

    some functionality updated

--- controllers/donationController.js
+++ controllers/donationController.js
@@ -1,16 +1,14 @@
 const Donation = require("../models/donationModel");
 
- const saveDonation = async (req, res) => {
-  try {
-    const { name, email, amount, paymentId } = req.body;
-
-    const donation = new Donation({ name, email, amount, paymentId });
-    await donation.save();
-
-    res.status(200).json({ message: "Donation saved successfully." });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to save donation." });
-  }
+const saveDonation = async (req, res) => {
+    try {
+        const { name, email, amount, address, razorpay_payment_id } = req.body;
+        const donation = new Donation({ name, email, amount, address, razorpay_payment_id });
+        await donation.save();
+        res.status(200).json({ success: true, message: "Donation saved successfully." });
+    } catch (err) {
+        res.status(500).json({ error: "Failed to save donation." });
+    }
 };
 
 module.exports = {
--- models/donationModel.js
+++ models/donationModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const donationSchema = new mongoose.Schema(
     {
-        name: String,
-        email: String,
-        amount: Number,
-        paymentId: String,
+        name: { type: String , require:true},
+        email: { type: String , require:true},
+        amount: { type: Number , require:true},
+        razorpay_payment_id:{ type: String , require:true},
         date: { type: Date, default: Date.now },
     },
     { timestamps: true }
